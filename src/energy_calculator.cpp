7adb869b988e0084ac4b5928e20c799a43d8dc84
Simonas <siminskas@gmail.com>
2024-02-21 16:49:46 +0200

    helper addCombs added

--- src/energy_calculator.cpp
+++ src/energy_calculator.cpp
@@ -10,10 +10,16 @@ using namespace energy_generator;
 
 int main(){
     Combs combs[7];
+    Combs *goodCombs;
     for (int i = 0; i < 7; i++)
     {
         std::cout << GEN_CONSTS::GEN_LIST[i].to_string() << std::endl;
         //std::cout << "Hello, World!" << std::endl;
     }
 
+}
+
+void addComb(Combs combs, std::map<int, int> newComb){
+    combs.combs[combs.length+1] = newComb;
+    combs.length++;
 }
